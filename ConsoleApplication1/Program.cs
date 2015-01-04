9425a72aa894c237656f9c50779fbb7f7300192f
fredokapo <fred.dessallien@gmail.com>
2015-01-04 22:02:08 +0100

    refactoring

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -5,56 +5,14 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.ServiceModel.Description;
 using System.Text;
-using WcfService1; 
+using WcfService1;
 
 namespace ConsoleApplication1
 {
-    //public class Program
-    //{
-    //    public static void Main(string[] args)
-    //    {
-    //        Uri baseAddress = new Uri("http://localhost:8090/MonitorService");
+  
+   
 
-    //        // Create the ServiceHost.
-    //        WebServiceHost host = new WebServiceHost(typeof(TVRemoteProxy),  baseAddress);
-    //        ServiceEndpoint ep = host.AddServiceEndpoint(typeof(ITVRemoteProxy), new WebHttpBinding(), baseAddress);
-    //        ServiceDebugBehavior stp = host.Description.Behaviors.Find<ServiceDebugBehavior>();
-    //        stp.HttpHelpPageEnabled = false;
-    //        host.Open();
-    //        Console.WriteLine("Service is up and running");
-    //        Console.WriteLine("Press enter to quit ");
-    //        Console.ReadLine();
-    //        host.Close();
-
-
-            
-    //        //using (ServiceHost host = new ServiceHost(typeof(TVRemoteProxy)))//, baseAddress))
-    //        //{
-    //            //// Enable metadata publishing.:
-    //            //ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
-    //            //smb.HttpGetEnabled = true;
-
-    //            //smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
-    //            //host.Description.Behaviors.Add(smb);
-
-    //            ////// Open the ServiceHost to start listening for messages. Since
-    //            ////// no endpoints are explicitly configured, the runtime will create
-    //            ////// one endpoint per base address for each service contract implemented
-    //            ////// by the service.
-    //            //host.Open();
-
-    //            //Console.WriteLine("The service is ready at {0}", baseAddress);
-    //            //Console.WriteLine("Press <Enter> to stop the service.");
-    //            //Console.ReadLine();
-
-    //            //// Close the ServiceHost.
-    //            //host.Close()
-    //    //    }
-    //    }
-    //}
-
-
-    sealed class Program : ITVRemoteProxy 
+    sealed class Program : ITVRemoteProxy
     {
 
         static void Main(string[] args)
@@ -62,12 +20,10 @@ namespace ConsoleApplication1
 
             Uri baseAddress = new Uri("http://localhost:8090");
 
-            HostWithServiceHost(baseAddress);
-
-            HostWithWebServiceHost(baseAddress);
+            StartServiceHost(baseAddress);
         }
 
-        private static void HostWithServiceHost(Uri baseAddress)
+        private static void StartServiceHost(Uri baseAddress)
         {
 
             ServiceHost host = new ServiceHost(typeof(Program), baseAddress);
@@ -75,31 +31,18 @@ namespace ConsoleApplication1
 
             ServiceEndpoint endpoint = host.AddServiceEndpoint(typeof(ITVRemoteProxy), binding, "ServiceHost");
             WebHttpBehavior httpBehavior = new WebHttpBehavior();
-                httpBehavior.DefaultOutgoingRequestFormat = WebMessageFormat.Json ;
-                httpBehavior.DefaultBodyStyle = WebMessageBodyStyle.Bare;
-                binding.CrossDomainScriptAccessEnabled = true;
+            httpBehavior.DefaultOutgoingRequestFormat = WebMessageFormat.Json;
+            httpBehavior.DefaultBodyStyle = WebMessageBodyStyle.Bare;
+            binding.CrossDomainScriptAccessEnabled = true;
             endpoint.Behaviors.Add(httpBehavior);
 
             host.Open();
 
-            Console.WriteLine(@"go to http://localhost:8090/ServiceHost/SomeOperation to test");
+            Console.WriteLine(@"go to http://localhost:8090/ServiceHost/GetHostInfos to test");
             Console.ReadLine();
 
         }
 
-        private static void HostWithWebServiceHost(Uri baseAddress)
-        {
-
-            WebServiceHost host = new WebServiceHost(typeof(Program), baseAddress);
-            WebHttpBinding binding = new WebHttpBinding();
-            host.AddServiceEndpoint(typeof(ITVRemoteProxy), binding, "WebServiceHost");
-            host.Open();
-            binding.CrossDomainScriptAccessEnabled = true;
-            Console.WriteLine(@"go to http://localhost:8090/WebServiceHost/SomeOperation to test");
-            Console.ReadLine();
-            host.Close();
-
-        }
 
         public HostInfo GetHostInfos()
         {
